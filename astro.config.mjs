7002f06a99a0a706f15d64b306f0c06d319b9f58
nestorhoracio <140070257+nestorhoracio@users.noreply.github.com>
2026-05-31 10:04:04 -0300

    config: URL real de Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import netlify from '@astrojs/netlify';
 export default defineConfig({
   output: 'hybrid',
   adapter: netlify(),
-  site: 'https://empaquecentro.netlify.app',
+  site: 'https://centroempaque.netlify.app',
 });
